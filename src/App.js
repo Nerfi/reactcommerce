2602838dd6c5e100e9c9c65b0c078105fb68d53c
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-03-24 22:29:35 +0100

    created UI folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/UI/Navbar';
+
+function App() {
+  return (
+   <>
+   <NavBar/>
+   wokirng
+
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return(
+    <p>working NAvbars</p>
+  )
+};
+
+export default NavBar;
