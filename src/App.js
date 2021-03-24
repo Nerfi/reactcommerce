f13de115fc89c5614e39fc6502b8272ad3f6289c
Juan Paredes <juanchuu.jfpc@gmail.com>
2021-03-24 22:46:18 +0100

    added almost all packages , less react-hook-form, added it later on

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/UI/Navbar';
+import LandingPage from './components/UI/LandingPage';
 
 function App() {
   return (
    <>
    <NavBar/>
+   <LandingPage/>
    wokirng
 
    </>
---
+++ src/components/UI/LandingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return(
+    <p>LandingPage</p>
+    )
+};
+
+export default LandingPage;
